5969a06d9ae9808fa700406d391ead0a2edbc6e9
p0lyw0lf <p0lyw0lf@protonmail.com>
2022-07-18 20:14:16 -0400

    Add new Lewd image that I did!

--- src/components/art/art_with_clickthrough.svelte
+++ src/components/art/art_with_clickthrough.svelte
@@ -2,9 +2,10 @@
   import TwitterLink from '../twitter_link.svelte';
 
   export let src;
-  export let artist = undefined;
+  export let artist;
 </script>
 
 <p>
   {#if artist }Comm by <TwitterLink username={artist} />. {/if}<a href={src.src}>Click Through To See</a>
+  <slot />
 </p>
