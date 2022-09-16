798fbd042c72f6148baaad039254bb1b06a32e53
p0lyw0lf <p0lyw0lf@protonmail.com>
2022-09-15 22:01:53 -0400

    Change to MDX for cybersec, add new posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,13 @@ import compress from 'astro-compress';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        integrations: true,
-    },
-    integrations: [
-        compress(),
-        image(),
-        svelte(),
-        sitemap(),
-    ],
-    site: 'https://wolfgirl.dev',
+  experimental: {
+    integrations: true
+  },
+  integrations: [compress(), image(), svelte(), sitemap(), mdx()],
+  site: 'https://wolfgirl.dev'
 });
