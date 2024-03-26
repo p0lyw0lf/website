f6124fbb9d9bb9aaddfc279c38416880a8ec9040
p0lyw0lf <p0lyw0lf@protonmail.com>
2024-03-25 21:40:35 -0400

    github workflow caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
@@ -7,5 +7,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), sitemap(), mdx(), compress()],
-  site: 'https://wolfgirl.dev'
+  site: 'https://wolfgirl.dev',
+
+  cacheDir: "./.astro-cache",
 });
