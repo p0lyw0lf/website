80bc5e51cfd77f2976a73d0c07359efd3cb1a8d5
p0lyw0lf <p0lyw0lf@protonmail.com>
2024-04-06 19:22:42 -0400

    different astro-compress ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import {defineConfig} from 'astro/config';
-import compress from 'astro-compress';
-import sitemap from '@astrojs/sitemap';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import compress from "@playform/compress";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), sitemap(), mdx(), compress()],
-  site: 'https://wolfgirl.dev',
+  site: "https://wolfgirl.dev",
 
   cacheDir: "./.astro-cache",
 });
