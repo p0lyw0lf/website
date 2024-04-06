713a458f73ebc18511cdd386d6547b0c15bea21a
p0lyw0lf <p0lyw0lf@protonmail.com>
2024-04-06 19:24:18 -0400

    nah just remove it entirely

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
-import compress from "@playform/compress";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), sitemap(), mdx(), compress()],
+  integrations: [svelte(), sitemap(), mdx()],
   site: "https://wolfgirl.dev",
 
   cacheDir: "./.astro-cache",
