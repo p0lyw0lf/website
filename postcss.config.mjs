5e77a7dab6abaaaaf3c9c4578a8566824f622367
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2024-10-03 21:57:31 -0400

    mobile blog post styling

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
 import autoprefixer from "autoprefixer";
 import cssnanoPlugin from "cssnano";
 import nesting from "postcss-nesting";
-import css_variables from "postcss-css-variables";
 
 export default {
-  plugins: [nesting({}), css_variables({}), autoprefixer, cssnanoPlugin],
+  plugins: [nesting({}), autoprefixer, cssnanoPlugin],
 };
