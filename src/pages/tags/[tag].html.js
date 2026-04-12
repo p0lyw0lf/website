87dde1a0c0fda525a01cb71f60458be4f4e709db
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2026-04-12 19:49:42 -0400

    tag pages
    proceeding smoothly :)
    
    cybersec pages and art gallery remains, but i think these should be
    fairly easy as well.

---
+++ src/pages/tags/[tag].html.js
@@ -0,0 +1,55 @@
+import { run_task } from "driver";
+import { PostLink } from "../../components/blog/PostLink.js";
+import { toTagUrl } from "../../data/urls.js";
+import { html } from "../../render.js";
+import { Post } from "../../templates/Post.js";
+
+const getTags = async () => {
+  const pages = await run_task("src/pages/blog/[slug].html.js", null);
+
+  const byTag = new Map();
+  for (const page of pages) {
+    for (const tag of page.frontmatter.tags) {
+      const existing = byTag.get(tag) ?? [];
+      existing.push(page);
+      byTag.set(tag, existing);
+    }
+  }
+
+  return [...byTag.entries()].map(([tag, pages]) => ({
+    slug: tag,
+    pages: pages.reverse(),
+  }));
+};
+
+const buildTagPage = async ({ slug: tag, pages }) => {
+  const url = toTagUrl(tag);
+  return await Post({
+    pathname: url,
+    homeLink: "/blog/",
+    sectionTitle: "PolyWolf's Blog",
+    rssLink: `/tags/${tag}/rss.xml`,
+    title: `#${tag}`,
+    description: `All my blog posts tagged with #${tag}`,
+  })(html`
+    <span style="display: inline-flex; align-items: center; gap: 1rem;">
+      <h1>#${tag}</h1>
+      <a href="/tags/">All Tags</a>
+    </span>
+    <ul>
+      ${pages.map(
+        (page) =>
+          html`<li
+            >${PostLink({
+              title: page.frontmatter.title,
+              published: page.frontmatter.published,
+              tags: page.frontmatter.tags,
+              slug: page.slug,
+            })}</li
+          >`,
+      )}
+    </ul>
+  `);
+};
+
+export default ARG ? await buildTagPage(ARG) : await getTags();
---
+++ src/pages/tags/index.html.js
@@ -0,0 +1,31 @@
+import { run_task } from "driver";
+import { TagLink } from "../../components/blog/TagLink.js";
+import { html } from "../../render.js";
+import { Post } from "../../templates/Post.js";
+
+const tags = await run_task("src/pages/tags/[tag].html.js", null);
+const sortedTags = tags.sort(
+  ({ slug: tagA, pages: pagesA }, { slug: tagB, pages: pagesB }) => {
+    const byNumPages = pagesB.length - pagesA.length;
+    if (byNumPages) return byNumPages;
+    const byTagName = tagA.localeCompare(tagB);
+    return byTagName;
+  },
+);
+
+export default await Post({
+  pathname: "/tags/",
+  homeLink: "/blog/",
+  sectionTitle: "PolyWolf's Blog",
+  title: "Tags",
+  description:
+    "A list of all tags on my site, which lead you to blog posts tagged with those tags!",
+})(html`
+  <h1>Tags</h1>
+  <ul>
+    ${sortedTags.map(
+      ({ slug: tag, pages }) =>
+        html`<li>${TagLink({ tag })} (${pages.length})</li>`,
+    )}
+  </ul>
+`);
