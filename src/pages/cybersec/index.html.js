718dc7f01f1cfd915c87f5aaae65c42969f89f91
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2026-06-07 22:45:06 -0400

    add rss link to cybersec

--- src/pages/cybersec/index.html.js
+++ src/pages/cybersec/index.html.js
@@ -1,9 +1,11 @@
-import { run_js } from "driver";
+import { read_file, run_js } from "driver";
 import { Post } from "../../components/blog/Post.js";
 import { CybersecLink } from "../../components/cybersec/CybersecLink.js";
-import { html } from "../../render.js";
+import { SITE_URL } from "../../data/urls.js";
+import { css, html } from "../../render.js";
 
 const posts = await run_js("src/pages/cybersec/[slug].html.js", null);
+const rssLink = `${SITE_URL}/cybersec/rss.xml`;
 
 export default await Post({
   sectionTitle: "PolyWolf on Security",
@@ -11,32 +13,43 @@ export default await Post({
   title: "Cybersecurity",
   description:
     'A list of links that PolyWolf has curated to be "cool" and under the vauge collection of having to do with computer security',
-})(html`
-  <div class="info">
-    <h1 class="p-name">Cybersec</h1>
-  </div>
-  <article>
-    <h1>NONE OF THIS CONTENT IS MY OWN</h1>
-    <p>
-      This is a list of links that I've been posting to a random Discord channel
-      for a cybersecurity class I was in. The server is fairly inactive (but ppl
-      still read the channel!) and keeping things exclusively on Discord is bad,
-      so I wrote a
-      <a href="https://github.com/p0lyw0lf/crossposter">Discord bot</a> that
-      mirrors activity in the channel to this page and a
-      <a href="https://infosec.exchange/@PolyWolf">Mastodon account</a>.
-    </p>
-    <ul>
-      ${posts.map(
-        (post) =>
-          html`<li>
-            ${CybersecLink({
-              slug: post.slug,
-              title: post.frontmatter.title,
-              repost_link: post.frontmatter.repost_link,
-            })}
-          </li>`,
-      )}
-    </ul>
-  </article>
-`);
+})(
+  html`
+    <div class="info">
+      <h1 class="p-name">Cybersec</h1>
+    </div>
+    <article>
+      <h1>NONE OF THIS CONTENT IS MY OWN</h1>
+      <p>
+        This is a list of links that I've been posting to a random Discord
+        channel for a cybersecurity class I was in. The server is fairly
+        inactive (but ppl still read the channel!) and keeping things
+        exclusively on Discord is bad, so I wrote a
+        <a href="https://github.com/p0lyw0lf/crossposter">Discord bot</a> that
+        mirrors activity in the channel to this page, a
+        <a href="https://infosec.exchange/@PolyWolf">Mastodon account</a>, &amp;
+        an
+        <a href="${rssLink}" id="rss"
+          >RSS feed ${await read_file("public/rss.svg")}</a
+        >
+      </p>
+      <ul>
+        ${posts.map(
+          (post) =>
+            html`<li>
+              ${CybersecLink({
+                slug: post.slug,
+                title: post.frontmatter.title,
+                repost_link: post.frontmatter.repost_link,
+              })}
+            </li>`,
+        )}
+      </ul>
+    </article>
+  `.withStyle(css`
+    #rss svg {
+      height: 1em;
+      margin-left: 0.25em;
+    }
+  `),
+);
